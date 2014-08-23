ca0f3b50a8eef8ef437f3d97e142bbd0035fc34d
Roee Shenberg <shenberg@gmail.com>
2014-08-23 07:56:24 -0700

    fixed fixed-point sines code

--- bootstest/Sines.h
+++ bootstest/Sines.h
@@ -27,7 +27,8 @@ prog_uchar PROGMEM sinetable[256] = {
 
 class Sines : public Animation {
 public:
-	Sines(MultiNeoPixel& strip, bool active) : Animation(strip, active) {
+	Sines(MultiNeoPixel& strip, bool active) : Animation(strip, active) 
+	{
 	}
 
 protected:
@@ -38,11 +39,16 @@ protected:
 public:
 	void sinRainbow() {
 		uint32_t currentTime = getTime();
-		for(uint16_t x=0; x < mStrip.getSizeX(); x++) {
-			for(uint16_t y=0; y < mStrip.getSizeY(); y++) {
-				int val = 64*(sin((x*131 + currentTime)/151.f) + sin((y*71 + currentTime)/131.f));
-				int val2 = 384 + 192*(sin((x*113 - currentTime)/131.f) + sin((y*31 + currentTime)/151.f));
+		int32_t t = currentTime >> 11;
+		for(int32_t x=0; x < mStrip.getSizeX(); x++) {
+			for(int32_t y=0; y < mStrip.getSizeY(); y++) {
+				int val = (sine((x*571 + t)/23) + sine((y*353 + t)/27) - 255);
+				int val2 = 3*(sine((x*153 + t)/43) + sine((y*61 + t)/27)) >> 1;
+				//int val = 64*(sin((x*131 + currentTime)/151.f) + sin((y*71 + currentTime)/131.f));
+				//int val2 = 384 + 192*(sin((x*113 - currentTime)/131.f) + sin((y*31 + currentTime)/151.f));
 				//int val3 = 64*(sin((x*11 + getFrameCount())/27.f) + sin((y*11 - getFrameCount())/17.f));
+				//int val = 64*(sin((x*131 + t)/151.f) + sin((y*71 + t)/131.f));
+				//int val2 = 384 + 192*(sin((x*113 - t)/131.f) + sin((y*31 + t)/151.f));
 				mStrip.setPixelColor(x,y, Wheel(val2, max(val-15, 0)));
 			}
 		}
@@ -54,7 +60,9 @@ public:
 		mIsActive = 0;
 	}
 
-	//inline byte sine(byte offset, )
+	inline byte sine(byte offset) {
+		return pgm_read_byte_near(&(sinetable[offset]));
+	}
 
 private:
 };
