a776c849f581805c0c0110e45bb6bec9062b5ad4
Amir Eshel <amire@everything.me>
2014-09-29 00:31:33 +0300

    Motion now stores a time window of the past 16 samples, accessible by getSample(offset)

--- bootstest/Motion.h
+++ bootstest/Motion.h
@@ -2,29 +2,34 @@
 #define _MOTION_H_
 
 #include "MPU6050.h"
+#include "MotionSample.h"
+
+#define WINDOW_SIZE		16
 
 class Motion {
 public:
-	Motion() : mSensor(0x68) {
-		ax = ay = az = 0;
-		gx = gy = gz = 0;
-		apower = 0;
+	Motion() : mSensor(0x68), mWindowSize(WINDOW_SIZE) {
+		clearBuffer();
 	}
 
 	~Motion() {
 
 	}
 
-	void begin() {
-		ax = ay = az = 0;
-		gx = gy = gz = 0;
-		apower = 0;
-  		mSensor.initialize();
-  		mSensor.setFullScaleAccelRange(MPU6050_ACCEL_FS_8);
+	void clearBuffer() {
+		mWindowHead = mWindowSize - 1;
+		mSamplesCollected = 0;
+	}
+
+	void print() {
+		Serial.print("samples=");
+		Serial.print(mSamplesCollected);
+		Serial.print(", head=");
+		Serial.println(mWindowHead);
 	}
 
 	void sample() {
-		// read raw accel/gyro measurements from device
+		int16_t ax, ay, az, gx, gy, gz;
 		mSensor.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
 		// 1G = 1024 (when in 8G mode)
@@ -32,24 +37,41 @@ public:
 		ay >>= 2;
 		az >>= 2;
 
-		float fx = (float)ax;
-		float fy = (float)ay;
-		float fz = (float)az;
+		if (mSamplesCollected < mWindowSize) {
+			mSamplesCollected++;
+		}
+		mWindowHead = (mWindowHead + 1) % mWindowSize;		
 
-		apower = (int16_t)sqrt(fx*fx + fy*fy + fz*fz);
+		mSamples[mWindowHead].set(ax, ay, az, gx, gy, gz);
 	}
 
-	void print() const {
-		// display tab-separated accel/gyro x/y/z values
-		Serial.print("a/g:\t");
-		Serial.print(ax); Serial.print('\t');
-		Serial.print(ay); Serial.print('\t');
-		Serial.print(az); Serial.print('\t');
-		Serial.print(apower); Serial.print('\t');
-		Serial.print(gx); Serial.print('\t');
-		Serial.print(gy); Serial.print('\t');
-		Serial.print(gz); Serial.print('\t');
-		Serial.println();
+	const uint16_t getWindowSize() const {
+		return mWindowSize;
+	}
+
+	const uint16_t getSamplesCount() const {
+		return mSamplesCollected;
+	}
+
+	const MotionSample& getSample(uint16_t offset = 0) const {
+		if (offset > mSamplesCollected) {
+			offset = mSamplesCollected;
+		}
+
+		uint16_t sampleIndex = mWindowHead;
+		if (sampleIndex >= offset) {
+			sampleIndex = sampleIndex - offset;
+		} else {
+			sampleIndex = mWindowSize + sampleIndex - offset;
+		}
+
+		return mSamples[sampleIndex];
+	}
+
+	void begin() {
+		clearBuffer();
+  		mSensor.initialize();
+  		mSensor.setFullScaleAccelRange(MPU6050_ACCEL_FS_8);
 	}
 
 	bool test() {
@@ -57,39 +79,41 @@ public:
 	}
 
 	inline int16_t getAPower() const {
-		return apower;
+		return getSample().apower;;
 	}
 
 	inline int16_t getAX() const {
-		return ax;
+		return getSample().ax;
 	}
 
 	inline int16_t getAY() const {
-		return ay;
+		return getSample().ay;
 	}
 
 	inline int16_t getAZ() const {
-		return az;
+		return getSample().az;
 	}
 
 	inline int16_t getGX() const {
-		return gx;
+		return getSample().gx;
 	}
 
 	inline int16_t getGY() const {
-		return gy;
+		return getSample().gy;
 	}
 
 	inline int16_t getGZ() const {
-		return gz;
+		return getSample().gz;
 	}
 
 private:
 	MPU6050 mSensor;
 
-	int16_t ax, ay, az;
-	int16_t apower;
-	int16_t gx, gy, gz;
+	MotionSample mSamples[WINDOW_SIZE];
+
+	const uint16_t mWindowSize;
+	uint16_t mWindowHead;
+	uint16_t mSamplesCollected;
 };
 
 #endif // #ifndef _MOTION_H_
